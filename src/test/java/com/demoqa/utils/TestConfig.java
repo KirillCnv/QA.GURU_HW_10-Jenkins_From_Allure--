7b0b77758923e5043eb770d42748cd62ddc326c8
Kirill <kirillcnv@gmail.com>
2022-09-06 01:09:52 +0300

    refactoring

--- src/test/java/com/demoqa/utils/TestConfig.java
+++ src/test/java/com/demoqa/utils/TestConfig.java
@@ -1,15 +1,19 @@
 package com.demoqa.utils;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestConfig extends TestData {
     @BeforeAll
     static void configure() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "0x0";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
 }
